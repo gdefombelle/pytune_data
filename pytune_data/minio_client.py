f3a46f312fa1a3b7eb9e0dce4728da6872354dbf
gdefombelle <gabriel.de.fombelle@gmail.com>
2025-12-08 22:04:38 +0100

    docker + deploy

--- pytune_data/minio_client.py
+++ pytune_data/minio_client.py
@@ -1,23 +1,26 @@
-import os
 from minio import Minio
 from pytune_configuration.sync_config_singleton import config, SimpleConfig
 
 config = config or SimpleConfig()
 
-MINIO_ENDPOINT = config.MINIO_ENDPOINT
+MINIO_ENDPOINT = config.MINIO_ENDPOINT  # "minio:9000"
 MINIO_ACCESS_KEY = config.MINIO_ACCESS_KEY
 MINIO_SECRET_KEY = config.MINIO_SECRET_KEY
+
 TEMP_BUCKET_NAME = config.MINIO_TEMP_BUCKET
-PIANO_SESSION_IMAGES_BUCKET = config.MINIO_PIANO_SESSION_BUCKET
-PIANO_SESSIONS_PDF_BUCKET = config.MINIO_PIANO_SESSIONS_PDF_BUCKET
+PIANO_SESSION_IMAGES_BUCKET = config.PIANO_SESSION_IMAGES_BUCKET
+PIANO_SESSIONS_PDF_BUCKET = config.PIANO_SESSIONS_PDF_BUCKET
 
 class MinioClient:
     def __init__(self):
+        endpoint = MINIO_ENDPOINT.replace("http://", "").replace("https://", "")
+        
         self.client = Minio(
-            MINIO_ENDPOINT.replace("http://", "").replace("https://", ""),
+            endpoint,
             access_key=MINIO_ACCESS_KEY,
             secret_key=MINIO_SECRET_KEY,
-            secure=False  # ⚠️ à adapter pour HTTPS plus tard
+            secure=False,
+            region="us-east-1"  # 👈 supprime le warning location=
         )
 
     def ensure_bucket_exists(self):
